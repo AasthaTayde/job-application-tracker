43dbbe8367db731c11ffdd6c405f0302dd5093b2
Aastha <aasthaktayde@gmail.com>
2026-05-02 00:15:52 +0530

    AddJob Form added with required inputs

--- frontend/JobApplication-Tracker/src/pages/AddJob.jsx
+++ frontend/JobApplication-Tracker/src/pages/AddJob.jsx
@@ -1,8 +1,72 @@
+import { useState } from "react";
+
 export default function AddJob() {
-    return (
-      <div>
-        <h1>Add Job</h1>
-        <p>Form will be built here</p>
-      </div>
-    );
-  }
+  const [job, setJob] = useState({
+    company: "",
+    role: "",
+    status: "Applied",
+    date: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(job);
+
+    // reset form
+    setJob({
+      company: "",
+      role: "",
+      status: "Applied",
+      date: "",
+    });
+  };
+
+  return (
+    <div>
+      <h1>Add Job</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Company"
+          value={job.company}
+          onChange={(e) =>
+            setJob({ ...job, company: e.target.value })
+          }
+        />
+
+        <input
+          type="text"
+          placeholder="Role"
+          value={job.role}
+          onChange={(e) =>
+            setJob({ ...job, role: e.target.value })
+          }
+        />
+
+        <select
+          value={job.status}
+          onChange={(e) =>
+            setJob({ ...job, status: e.target.value })
+          }
+        >
+          <option>Applied</option>
+          <option>Interview</option>
+          <option>Offer</option>
+          <option>Rejected</option>
+        </select>
+
+        <input
+          type="date"
+          value={job.date}
+          onChange={(e) =>
+            setJob({ ...job, date: e.target.value })
+          }
+        />
+
+        <button type="submit">Add Job</button>
+      </form>
+    </div>
+  );
+}
