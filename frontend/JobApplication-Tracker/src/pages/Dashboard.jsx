8ec723a7c402c1a914942f0a20e8a36b9d88402e
Aastha <aasthaktayde@gmail.com>
2026-05-05 19:39:50 +0530

    Added update functionality with use of dropdown at dashboard page

--- frontend/JobApplication-Tracker/src/pages/Dashboard.jsx
+++ frontend/JobApplication-Tracker/src/pages/Dashboard.jsx
@@ -2,7 +2,13 @@ export default function Dashboard({ jobs,setJobs }) {//jobs->shows data, setJobs
   const handleDelete = (index) => {
     const updatedJobs = jobs.filter((_, i) => i !== index);//create new array and this keeps all items except the one we want to delete, filter() creates new array
     setJobs(updatedJobs);//replace old jobs with new array
+  };          
+    const handleStatusChange = (index, newStatus) => {
+      const updatedJobs = [...jobs]; // copy array
+      updatedJobs[index].status = newStatus; // update specific job
+      setJobs(updatedJobs); // update state
   };
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -14,7 +20,14 @@ export default function Dashboard({ jobs,setJobs }) {//jobs->shows data, setJobs
           <div key={index}>
             <h3>{job.company}</h3>
             <p>{job.role}</p>
-            <p>{job.status}</p>
+            <select
+            value={job.status}
+            onChange={(e) => handleStatusChange(index, e.target.value)}>
+            <option>Applied</option>
+            <option>Interview</option>
+            <option>Offer</option>
+            <option>Rejected</option>
+            </select>
             <p>{job.date}</p>
             <button onClick={() => handleDelete(index)}>
             Delete
